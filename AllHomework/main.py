499449b97f77b2ce3259873d5365b6527e489ffd
 ÐAliaskar2255 <aliaskar2255@gmail.com>
2024-07-20 00:58:55 +0600

    Ваше сообщение коммита

--- AllHomework/main.py
+++ AllHomework/main.py
@@ -8,6 +8,7 @@ from handlers.recipe import recipe_router
 from handlers.echo import echo_router
 from handlers.dishes import dishes_router
 from handlers.review_dialog import review_router
+from handlers.menu import menu_router
 
 async def on_startup(bot: Bot):
     database.create_tables()
@@ -19,6 +20,7 @@ async def main():
     dp.include_router(my_info_router)
     dp.include_router(recipe_router)
     dp.include_router(dishes_router)
+    dp.include_router(menu_router)
     # В конце
     dp.include_router(echo_router)
     # При запуске
