7eb77f4368a552795c4098f7924b8bb1e50177c4
 ÐAliaskar2255 <aliaskar2255@gmail.com>
2024-07-19 16:05:07 +0600

    asd

--- AllHomework/database/queries.py
+++ AllHomework/database/queries.py
@@ -10,3 +10,20 @@ class Queries:
         extra_comments TEXT
     )
     """
+
+    CREATE_CATEGORY_OF_DISHES_TABLE = """
+    CREATE TABLE IF NOT EXISTS category_dishes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    )
+    """
+
+    CREATE_DISHES_TABLE = """
+    CREATE TABLE IF NOT EXISTS dishes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price INTEGER,
+        category_of_dishes INTEGER,
+        FOREIGN KEY (category_of_dishes) REFERENCES category_dishes(id)
+    )
+    """
